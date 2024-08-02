c93f6d4da5a48f2f6c8e4bac21aef028087fc853
DandiDamanik <dandidamanik210497@gmail.com>
2024-08-02 11:37:52 +0700

    mmebuat project test programming mitra sinerji

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MitraSinerji</title>
+</head>
+<body>
+    <h1>Test Programming</h1> <br>
+    <h1>Mitra Sinerji Teknoindo</h1>
+    
+</body>
+</html>
